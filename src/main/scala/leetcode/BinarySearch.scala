b790278b898b4cf7ca13ba2636b0364a8d3ac751
John Landahl <john@landahl.org>
2021-10-14 15:36:23 -0700

    Removed vars

--- src/main/scala/leetcode/BinarySearch.scala
+++ src/main/scala/leetcode/BinarySearch.scala
@@ -2,30 +2,25 @@ package leetcode
 
 object BinarySearch {
   def main(args: Array[String]): Unit = {
-    println(search(Array(1, 4, 7, 9, 12), 0))
+    println(search(Array(1, 4, 7, 9, 12), 7))
   }
 
   def search(nums: Array[Int], target: Int): Int = {
-    var left = 0
-    var right = nums.length - 1
     @annotation.tailrec
-    def loop(): Int = {
+    def loop(left: Int, right: Int): Int = {
       if (left > right) {
         -1
       } else {
         val mid = (right - left) / 2 + left
         val candidate = nums(mid)
-        if (candidate == target) {
+        if (candidate == target)
           mid
-        } else if (candidate < target) {
-          left = mid + 1
-          loop()
-        } else {
-          right = mid - 1
-          loop()
-        }
+        else if (candidate < target)
+          loop(mid + 1, right)
+        else
+          loop(left, mid - 1)
       }
     }
-    loop()
+    loop(0, nums.length - 1)
   }
 }
