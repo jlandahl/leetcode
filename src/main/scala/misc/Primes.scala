d0f58b332d660f2836ce0cb3a0fcfb8b71bfb012
John Landahl <john@landahl.org>
2021-10-15 13:02:06 -0700

    Minor refactoring

--- src/main/scala/misc/Primes.scala
+++ src/main/scala/misc/Primes.scala
@@ -4,10 +4,13 @@ object Primes extends App {
   val primes: LazyList[Int] =
     2 #:: LazyList.from(3, 2).filter(n => primes.takeWhile(p => p * p <= n).forall(p => n % p > 0))
 
-  def prime(n: Int): Int = {
+  /**
+   * Number of primes less than or equal to a given number
+   */
+  def primeCount(n: Int): Int = {
     primes.takeWhile(_ <= n).size
   }
 
   println(primes.takeWhile(_ <= 12).toList) // List(2, 3, 5, 7, 11)
-  println(prime(12)) // 5
+  println(primeCount(12)) // 5
 }
