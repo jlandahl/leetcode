eb8a09c92c2082484f9dcf154c0495d51b2ba27d
John Landahl <john@landahl.org>
2021-10-15 00:13:04 -0700

    Refactored to use tail recursion to avoid explicit return

--- src/main/scala/leetcode/TwoSum.scala
+++ src/main/scala/leetcode/TwoSum.scala
@@ -1,16 +1,25 @@
 package leetcode
 
-object TwoSum {
+object TwoSum extends App {
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
-    val withIndex = nums.zipWithIndex
-    val map = withIndex.toMap
-    withIndex.foreach { case (num, i) =>
-      val complement = target - num
-      val i2 = map.getOrElse(complement, -1)
-      if ((i2 >= 0) && (i2 != i)) {
-        return Array(i, i2)
+    val map = nums.zipWithIndex.toMap
+    @annotation.tailrec
+    def loop(i: Int): Array[Int] = {
+      if (i >= nums.length) {
+        Array.empty[Int]
+      } else {
+        val num = nums(i)
+        val complement = target - num
+        val i2 = map.getOrElse(complement, -1)
+        if ((i2 >= 0) && (i2 != i)) {
+          Array(i, i2)
+        } else {
+          loop(i + 1)
+        }
       }
     }
-    null
+    loop(0)
   }
+
+  println(twoSum(Array(1, 2, 3, 4, 5, 6, 7), 10).toList)
 }
