d452bbeba1ce05840373190f566ed4e5bbf08084
John Landahl <john@landahl.org>
2021-11-04 14:07:55 -0700

    Added problem description

--- src/main/scala/leetcode/BinarySearch.scala
+++ src/main/scala/leetcode/BinarySearch.scala
@@ -5,6 +5,12 @@ object BinarySearch {
     println(search(Array(1, 4, 7, 9, 12), 7))
   }
 
+  // Given an array of integers nums which is sorted in ascending order, and an integer target,
+  // write a function to search target in nums. If target exists, then return its index.
+  // Otherwise, return -1.
+  //
+  // You must write an algorithm with O(log n) runtime complexity.
+
   def search(nums: Array[Int], target: Int): Int = {
     @annotation.tailrec
     def loop(left: Int, right: Int): Int = {
